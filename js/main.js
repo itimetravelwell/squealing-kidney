19f47a63f2a15e7ad799212d21a4ee886557e9d9
itimetravelwell <charles.jackson@georgebrown.ca>
2015-10-02 14:35:01 -0400

    test

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 
 
+$('button').on('click',function(e) {
+	$('.light').toggleClass('dark');
+});
+
+$('button').on('click', function(e) {
+	$('button').toggleClass( "off ");
+
+});
+
+if ($("#button").hasClass("on off")) {
+  $("#status").animate({right: -700 +"px"}, 2000);
+}
+// button class is dark then change status text to "Hey, who turn off the lights?"
